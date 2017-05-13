3d6d17935d55eb14759707ed44edb95f76f9a6e0
sathya <narayanan177@rediffmail.com>
2017-05-13 11:13:36 +0530

    Changes done

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -10,7 +10,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            Console.WriteLine("Welcome to Test Project");
         }
     }
 }
